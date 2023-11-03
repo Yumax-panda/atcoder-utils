84a028f98af2bc33208aba22dacddd383008e7f0
Yumax-panda <yuma0wall@gmail.com>
2023-11-03 21:24:00 +0900

    test: sortedSet.add

--- tests/sortedSet.test.ts
+++ tests/sortedSet.test.ts
@@ -9,3 +9,15 @@ test('sortedSet', () => {
   expect(s.getItem(0)).toBe(1)
   expect(s.getItem(-1)).toBe(10)
 })
+
+test('sortedSet.add', () => {
+  const s = new SortedSet<number>({
+    array: [5, 3, 1, 10],
+  })
+  expect(s.add(5)).toBe(false)
+  expect(s.add(4)).toBe(true)
+  expect(s.size).toBe(5)
+  expect(s.has(4)).toBe(true)
+  expect(s.getItem(1)).toBe(3)
+  expect(s.getItem(2)).toBe(4)
+})
